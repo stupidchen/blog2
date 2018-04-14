8962e72cace43b20b28472f946a6aa1601c84fdb
Mike Chen <stupidchen@foxmail.com>
2018-04-14 18:02:08 +0800

    Finished: User, Comment and Archive models.

--- blog2/core/models.py
+++ blog2/core/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    id = models.CharField(max_length=64)
+    username = models.CharField(max_length=128)
+    email = models.CharField(max_length=128)
+    password = models.CharField(max_length=64)
+    information = models.CharField(max_length=1024)
+
+class Comment(models.Model):
+    author = models.CharField(max_length=128)
+    pubTime = models.DateTimeField()
+    context = models.CharField()
+
+class Archive(models.Model):
+    id = models.CharField(max_length=64)
+    title = models.CharField(max_length=512)
+    context = models.CharField()
+    author = models.CharField(max_length=128)
+    pubTime = models.DateTimeField()
+    editTime = models.DateTimeField()
+    comments = models.ForeignKey(Comment, on_delete=models.CASCADE)
