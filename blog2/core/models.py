317868e82aa2fc6c6aa230c72d1d8c96e2a56623
Mike Chen <stupidchen@foxmail.com>
2018-04-15 16:12:09 +0800

    Finished:Added initial database migration and the view stubs.

--- blog2/core/models.py
+++ blog2/core/models.py
@@ -2,22 +2,32 @@ from django.db import models
 
 # Create your models here.
 class User(models.Model):
-    id = models.CharField(max_length=64)
+    id = models.CharField(max_length=64, primary_key=True)
     username = models.CharField(max_length=128)
     email = models.CharField(max_length=128)
     password = models.CharField(max_length=64)
-    information = models.CharField(max_length=1024)
+    information = models.TextField()
+
+    def __str__(self):
+        return 'User ' + self.id + ' with name ' + self.username
 
 class Comment(models.Model):
+    id = models.CharField(max_length=64, primary_key=True)
     author = models.CharField(max_length=128)
     pubTime = models.DateTimeField()
-    context = models.CharField()
+    context = models.TextField()
+
+    def __str__(self):
+        return 'Comment ' + self.id + ' by ' + self.author
 
 class Archive(models.Model):
-    id = models.CharField(max_length=64)
-    title = models.CharField(max_length=512)
-    context = models.CharField()
+    id = models.CharField(max_length=64, primary_key=True)
+    title = models.TextField()
+    context = models.TextField()
     author = models.CharField(max_length=128)
     pubTime = models.DateTimeField()
     editTime = models.DateTimeField()
     comments = models.ForeignKey(Comment, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return 'Archive ' + self.id + 'by ' + self.author
