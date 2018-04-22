9973fd047af30a2eb970c7c6b8b546a808a5d47f
Mike Chen <stupidchen@foxmail.com>
2018-04-22 23:49:06 +0800

    Finished: Login templates.

--- blog2/core/urls.py
+++ blog2/core/urls.py
@@ -6,8 +6,8 @@ app_name = 'core'
 urlpatterns = [
     path('archive/', views.archive, name='archive'),
     path('archive/<str:aid>/', views.archive, name='archive_detail'),
-    path('user/login', views.login, name='user_login'),
-    path('user/logout', views.logout, name='user_logout'),
+    path('user/login', views.login, name='login'),
+    path('user/logout', views.logout, name='logout'),
     path('user/<str:uid>/', views.user, name='user_detail'),
     path('index/<str:uid>', views.index, name='index'),
 ]
