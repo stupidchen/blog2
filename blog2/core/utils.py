1921bee927262f2afc6523b1f47c6588b24eb1aa
Mike Chen <stupidchen@foxmail.com>
2018-04-27 23:26:41 +0800

    Finished: Implement the post method of UserView.

--- blog2/core/utils.py
+++ blog2/core/utils.py
@@ -4,7 +4,7 @@ def get_current_user():
     return 'root'
 
 # TODO: Thread safety
-class Tokens:
+class Tokens(object):
     __token_map__ = {}
 
     def __init__(self):
@@ -25,3 +25,24 @@ class Tokens:
     @classmethod
     def remove_token(cls, token):
         del cls.__token_map__[token]
+
+
+class Error(object):
+    __error_dict__ = {
+        0: 'No error',
+        1: 'Token invalid',
+        2: 'Username or password incorrect',
+        3: 'Invalid path',
+    }
+
+    UNKNOWN_ERROR = -1
+    NO_ERROR = 0
+    TOKEN_INVALID = 1
+    USERNAME_OR_PASSWORD_INCORRECT = 2
+    INVALID_PATH = 3
+
+    def get_error_message(self, code):
+        ret = 'Unknown error'
+        if code in self.__error_dict__:
+            ret = self.__error_dict__[code]
+        return ret
--- blog2/core/views/user.py
+++ blog2/core/views/user.py
@@ -1,6 +1,10 @@
 from django.http.response import JsonResponse
 from django.views.generic import View
 from ..models import User
+from ..utils import Tokens
+from ..utils import Error
+import json
+import uuid
 
 
 def get_url_param(path):
@@ -20,18 +24,55 @@ class UserView(View):
             return JsonResponse({
                 'data': data
             })
-        else:
-            user = User.objects.get(pk=get_url_param(path)['id'])
-            data = user.as_dict()
+
+        user = User.objects.get(pk=get_url_param(path)['id'])
+        data = user.as_dict()
+        return JsonResponse({
+            'error': Error.NO_ERROR,
+            'data': data,
+        })
+
+    def post(self, request, path):
+        if path == '/login':
+            data = json.loads(request.POST['data'])
+
+            username = data['username']
+            password = data['password']
+            try:
+                match_user = User.objects.get(username=username, password=password)
+            except:
+                return JsonResponse({
+                    'error': Error.USERNAME_OR_PASSWORD_INCORRECT,
+                })
+
+            token = uuid.uuid4()
+            Tokens.set_token(token, match_user.id)
             return JsonResponse({
-                'data': data
+                'error': Error.NO_ERROR,
+                'token': token,
             })
 
-    def post(self, request):
+        if path == '/logout':
+            token = request.POST['token']
+            try:
+                uid = Tokens.get_uid(token)
+            except:
+                return JsonResponse({
+                    'error': Error.TOKEN_INVALID,
+                })
+
+            Tokens.remove_token(token)
+            return JsonResponse({
+                'error': Error.NO_ERROR,
+            })
+
+        return JsonResponse({
+            'error': Error.INVALID_PATH,
+        })
+
+
+    def put(self, request, path):
         pass
 
-    def put(self, request):
-        pass
-
-    def delete(self, request):
+    def delete(self, request, path):
         pass
